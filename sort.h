5bf8676af36a79f8521891584484e1e9fccfd7c4
ammartica <amisadaymartinez@gmail.com>
2022-02-16 01:04:13 +0000

    added function prototypes

--- sort.h
+++ sort.h
@@ -28,6 +28,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quickSort(int *array, int low, int high, size_t size);
+size_t partition(int *array, int low, int high, size_t size);
+void swap(int *array, int a, int b);
 
 
 #endif /* _SORT_H_ */
